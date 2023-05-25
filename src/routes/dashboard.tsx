f83728a31ac46b55aeb9614ea58bfcf6aafcdbce
tiongMax <maxtiongcs16@gmail.com>
2023-05-25 23:11:12 +0800

    try

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Button, Typography } from "@mui/material";
+import { Button, CssBaseline, Stack, Typography } from "@mui/material";
 import supabase from "../supabase";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
 
@@ -11,11 +12,17 @@ export default function Dashboard() {
     }
 
     return (
+        
         <>
             <Typography variant="h3" component="h1">
                 Welcome back!
             </Typography>
-            <Button variant="contained" onClick={handleLogoutClick}>Log out</Button>
+            <Stack direction="row" gap={3}>
+                <Typography> <Link to="/timer"> timer </Link></Typography>
+                <Typography> <Link to="/timer"> reminder </Link></Typography>
+                <Typography> <Link to="/timer"> task </Link></Typography>
+                <Button variant="contained" onClick={handleLogoutClick}>Log out</Button>
+            </Stack>    
         </>
     );
 }
---
+++ src/routes/reminder.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import {Link} from "react-router-dom";
+
+export default function timer() {
+
+    document.title = "Reminder  // TODO Study";
+
+    return (
+        <Typography>This is a reminder.</Typography>
+    );
+}
