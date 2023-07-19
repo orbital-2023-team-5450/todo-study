f8a31f04977e7ab3712b27a864aacff6f9bfb2c9
Brendan Koh <brenstack62@gmail.com>
2023-07-19 23:39:03 +0800

    Update dashboard

--- src/components/dashboard/CanvasDownloaderPanel.tsx
+++ src/components/dashboard/CanvasDownloaderPanel.tsx
@@ -1,8 +1,8 @@
-import { Typography } from '@mui/material';
+import { Link, Typography } from '@mui/material';
 import React from 'react';
 
 export default function CanvasDownloaderPanel() {
   return (
-    <Typography>Hello world!</Typography>
+    <Typography>To download all your files from Canvas, go to the Canvas downloader feature <Link href="/canvas-downloader">here</Link>.</Typography>
   )
 }
